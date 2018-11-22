84904a2d92c8c11ede0c0f4efcae13b66eff091e
qqhbc <1162454662@qq.com>
2018-11-22 20:42:07 +0800

    修改bug

--- consumer-movie-feign/src/main/java/com/yc/cloud/consumermovie/feign/UserFeignClient.java
+++ consumer-movie-feign/src/main/java/com/yc/cloud/consumermovie/feign/UserFeignClient.java
@@ -1,6 +1,10 @@
 package com.yc.cloud.consumermovie.feign;
 
+import java.util.Map;
+
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,5 +14,17 @@ import com.yc.cloud.consumermovie.entity.User;
 @FeignClient(name="provider-user")
 public interface UserFeignClient {
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
-    public User findById(@RequestParam("id") Long id);
+    public User findById(@PathVariable("id") Long id);
+    // 该请求不会成功
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public User get0(User user);
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public User get1(@RequestParam("id") Long id, @RequestParam("username") String username);
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public User get2(@RequestParam Map<String, Object> map);
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public User post(@RequestBody User user);
 }
--- provider-user/src/main/java/com/yc/cloud/provideruser/controller/UserController.java
+++ provider-user/src/main/java/com/yc/cloud/provideruser/controller/UserController.java
@@ -3,6 +3,8 @@ package com.yc.cloud.provideruser.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yc.cloud.provideruser.entity.User;
@@ -12,11 +14,20 @@ import com.yc.cloud.provideruser.service.UserService;
 public class UserController {
     
     @Autowired
-    private UserService UserService;
+    private UserService userService;
     
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
-        return UserService.findById(id);
+        return userService.findById(id);
     }
     
+    @GetMapping("/get")
+    public User get(User user) {
+        return user;
+    }
+    
+    @PostMapping("/post")
+    public User post(@RequestBody User user) {
+        return user;
+    }
 }
